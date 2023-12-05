3365daad98e8467fc1f4a55850548506527f0ff1
Devdhan <danielbolaji99@gmail.com>
2023-12-05 01:04:34 +0100

    Update some UI

--- app/src/main/java/com/example/currencyconverter/SplashScreen.kt
+++ app/src/main/java/com/example/currencyconverter/SplashScreen.kt
@@ -1,17 +1,35 @@
 package com.example.currencyconverter
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.currencyconverter.databinding.FragmentSplashScreenBinding
 
 class SplashScreen : Fragment() {
+    private lateinit var binding: FragmentSplashScreenBinding
+    var handler: Handler? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        binding = FragmentSplashScreenBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        doSplashScreen()
+    }
+
+    private fun doSplashScreen() {
+        handler = Handler()
+        handler!!.postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_convertPage)
+        }, 3000)
     }
 }
